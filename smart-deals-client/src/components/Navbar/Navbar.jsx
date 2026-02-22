70d6c6ddce785586ac2af641d2c28658481ca728
md mehedi hassan <mdmehedi01326018868@gmail.com>
2026-02-22 23:56:46 +0600

    added

---
+++ smart-deals-client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,90 @@
+import React, { use } from "react";
+import { Link, NavLink } from "react-router";
+import { AuthContext } from "../../contexts/AuthContext";
+
+const Navbar = () => {
+  const {user,signOutUser}=use(AuthContext)
+  const handleSignOut=()=>{
+    signOutUser()
+    .then()
+    .catch()
+  }
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/allProducts">All Products</NavLink>
+      </li>
+      {/* <li><NavLink to='/register'>Register</NavLink></li> */}
+      {
+        user && <>
+        <li><NavLink to='/myProducts'>My Products</NavLink></li>
+        <li><NavLink to='/myBids'>My Bids</NavLink></li>
+        </>
+      }
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl flex items-center gap-2 font-black italic tracking-tighter hover:scale-105 transition-transform">
+          <div className="bg-blue-600 p-1.5 rounded-lg shadow-sm">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="white"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
+            </svg>
+          </div>
+          <span>
+            Smart<span className="text-blue-600">Deals</span>
+          </span>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+      {user ?  <a onClick={handleSignOut} className="btn">Sign Out</a>:
+       <Link to="/register" className="btn">Login</Link>
+      }
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
